58e21954e2d8c34aa27878dea930f9b88950a3cd
rbneiman <alecneiman@gmail.com>
2021-06-10 10:04:34 -0500

    changed url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Access database (*.mdb, *.accdb) parser",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ClarotyICS/access_parser",
+    url="https://github.com/rbneiman/access_parser",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
